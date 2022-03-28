6ec685bcda5b13a9ee538f2ec064af658d5b8b51
Halal37 <wojciechlidwin37@gmail.com>
2022-03-28 21:34:07 +0200

    Revert "Update sick_leave_screen.dart"
    This reverts commit bf69f2fdcca5879bd613b1ca04287eb966b0fe27.

--- lib/features/sick_leave/screens/sick_leave_screen.dart
+++ lib/features/sick_leave/screens/sick_leave_screen.dart
@@ -7,56 +7,6 @@ class SickLeaveScreen extends StatelessWidget {
     return SickLeaveCard(
       sickLeaveDescription: 'Zaświadczenie z dnia 05.11.2021',
       doctor: 'Let. Andrzej Mars',
-    return ListView(
-      padding: EdgeInsets.all(50),
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                'Zwolnienia',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontFamily: 'Manrope',
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-            ),
-            SickLeaveCard(
-              sickLeaveDescription: 'Zaświadczenie z dnia 05.11.2021',
-              doctor: 'Lek. Andrzej Mors',
-            ),
-            SizedBox(height: 15),
-            SickLeaveCard(
-              sickLeaveDescription: 'Zaświadczenie z dnia 21.07.2021',
-              doctor: 'Lek. Andrzej Mors',
-            ),
-            SizedBox(height: 15),
-            SickLeaveCard(
-              sickLeaveDescription: 'Zaświadczenie z dnia 15.02.2021',
-              doctor: 'Lek. Andrzej Mors',
-            ),
-            SizedBox(height: 15),
-            SickLeaveCard(
-              sickLeaveDescription: 'Zaświadczenie z dnia 17.12.2020',
-              doctor: 'Lek. Andrzej Mors',
-            ),
-            SizedBox(height: 15),
-            SickLeaveCard(
-              sickLeaveDescription: 'Zaświadczenie z dnia 11.08.2020',
-              doctor: 'Lek. Andrzej Mors',
-            ),
-            SizedBox(height: 15),
-            SickLeaveCard(
-              sickLeaveDescription: 'Zaświadczenie z dnia 13.03.2020',
-              doctor: 'Lek. Andrzej Mors',
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
